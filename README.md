1f21ba35b2a66a967a1b5a48406b2bd63280ff49
Celeste Lu <yl790@cam.ac.uk>
2021-03-15 14:21:46 +0000

    SIM-777: Second black formatting MR

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # SDP Workflow Library
 
+## Introduction
 The SDP workflow library is a high-level interface for writing workflows. Its
 goal is to provide abstractions to enable the developer to express the
 high-level organisation of a workflow without needing to interact directly with
 the low-level interfaces such as the SDP configuration library.
+
+
+## Contribute to this repository
+We use [Black](https://github.com/psf/black) to keep the python code style in good shape. 
+
+Please make sure you black-formatted your code before merging to master.
+
+The first step in the CI pipeline checks that the code complies with black formatting style,
+
+and will fail if that is not the case.
--- setup.py
+++ setup.py
@@ -11,30 +11,31 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 version = {}
-VERSION_PATH = os.path.join('src', 'ska_sdp_workflow', 'version.py')
-with open(VERSION_PATH, 'r') as file:
+VERSION_PATH = os.path.join("src", "ska_sdp_workflow", "version.py")
+with open(VERSION_PATH, "r") as file:
     exec(file.read(), version)
+
 
 def requirements_from(fname):
     """ Read requirements from a file. """
     with open(fname) as req_file:
-        return [ req for req in req_file.read().splitlines()
-                 if req[0] != '-' ]
+        return [req for req in req_file.read().splitlines() if req[0] != "-"]
+
 
 setuptools.setup(
-    name='ska-sdp-workflow',
-    version=version['__version__'],
-    description='SKA SDP Workflow',
-    author='Sim Team',
-    license='License :: OSI Approved :: BSD License',
+    name="ska-sdp-workflow",
+    version=version["__version__"],
+    description="SKA SDP Workflow",
+    author="Sim Team",
+    license="License :: OSI Approved :: BSD License",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    url='https://gitlab.com/ska-telescope/sdp/ska-sdp-workflow',
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/ska-telescope/sdp/ska-sdp-workflow",
     package_dir={"": "src"},
-    packages=setuptools.find_packages('src'),
-    install_requires=requirements_from('requirements.txt'),
-    setup_requires=['pytest-runner'],
-    tests_require=requirements_from('requirements-test.txt'),
+    packages=setuptools.find_packages("src"),
+    install_requires=requirements_from("requirements.txt"),
+    setup_requires=["pytest-runner"],
+    tests_require=requirements_from("requirements-test.txt"),
     zip_safe=False,
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -43,6 +44,6 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Astronomy",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: BSD License"
-    ]
+        "License :: OSI Approved :: BSD License",
+    ],
 )
